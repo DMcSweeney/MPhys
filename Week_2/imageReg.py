b0d2650dd57bb2698abb20bb30b959c704214bf6
Donal Mcsweeney <donalmcs1@gmail.com>
2018-10-09 16:00:33 +0100

    Donal Latest 9/10

--- Week_2/imageReg.py
+++ Week_2/imageReg.py
@@ -1,9 +1,14 @@
 # Class that contains all simpleITK functions required
 # for image resampling, as sanity checks for our codeself.
+from __future__ import print_function
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-mpl.rc('image', aspect ='equal')
+import os, sys
+import pydicom
+import numpy as np
+
+mpl.rc('image', aspect = 'equal')
 
 
 class ImageReg(object):
@@ -12,7 +17,64 @@ class ImageReg(object):
     def load_itk_image(pixel_array):
         img = sitk.GetImageFromArray(pixel_array)
         img = sitk.Cast(img, sitk.sitkFloat32)
+        pixel_array = sitk.GetArrayFromImage(img)
         print(img.GetSize())
+        print(img.GetDirection())
+        print(img.GetPixelIDTypeAsString())
         plt.imshow(pixel_array)
         plt.show()
         print("Done Loading Image")
+
+    @staticmethod
+    def load_series(data_directory, output_filename):
+        for filename in os.listdir(data_directory):
+            data_path = os.path.join(data_directory, filename)
+            ds = pydicom.dcmread(data_path)
+            series_ID = ds.SeriesInstanceUID
+        # Get the list of files belonging to a specific series IDself
+        reader = sitk.ImageSeriesReader()
+        # Use the functional interface to read the image series.
+        original_image = sitk.ReadImage(reader.GetGDCMSeriesFileNames(data_directory, series_ID))
+        # Write the image.
+        output_filename = os.path.join('..\\MPhys\\', '{}.mha'.format(output_filename))
+        sitk.WriteImage(original_image, output_filename)
+
+    @staticmethod
+    def image_info(img_series):
+        img = sitk.ReadImage(img_series)
+        print("Size:", img.GetSize())
+        print("Origin:", img.GetOrigin())
+        print("Pixel Spacing:", img.GetSpacing())
+        print("Direction:", img.GetDirection())
+        return img
+
+    @staticmethod
+    def resample_image(fixed, moving):
+        resample = sitk.ResampleImageFilter()
+        resample.SetReferenceImage(fixed)
+        resample.SetOutputOrigin(fixed.GetOrigin())
+        resample.SetOutputSpacing(moving.GetSpacing())
+        resample.SetSize(moving.GetSize())
+        resample.SetInterpolator(sitk.sitkBSpline)
+        resample.AddCommand(sitk.sitkProgressEvent, lambda: print("\rProgress: {0:03.1f}%...".format(100*resample.GetProgress()),end=''))
+        resample.AddCommand(sitk.sitkProgressEvent, lambda: sys.stdout.flush())
+        out = resample.Execute(moving)
+        print(type(out))
+        # vis = sitk.CheckerBoard(fixed, sitk.Compose([sitk.Cast(sitk.RescaleIntensity(out), sitk.sitkUInt8)]*3), checkerPattern =[15,10,1])
+        return out
+
+    @staticmethod
+    def myshow(img, title=None, margin=0.05, dpi=80):
+        # Function to display image
+        nda = sitk.GetArrayFromImage(img)
+        print(np.shape(nda))
+        spacing = img.GetSpacing()
+        ysize = nda.shape[1]
+        xsize = nda.shape[2]
+        figsize = (1 + margin) * ysize / dpi, (1 + margin) * xsize / dpi
+        fig = plt.figure(title, figsize=figsize, dpi=dpi)
+        ax = fig.add_axes([margin, margin, 1 - 2*margin, 1 - 2*margin])
+        extent = (0, xsize*spacing[1], 0, ysize*spacing[0])
+        t = ax.imshow(nda, extent=extent, interpolation='hamming', cmap='gray')
+        if(title):
+            plt.title(title)
--- Week_2/main.py
+++ Week_2/main.py
@@ -11,18 +11,22 @@ print(cwd)
 pct_path = ".\\Patients\\HN-CHUM-001\\08-27-1885-TomoTherapy Patient Disease-00441\\112161818-kVCT Image Set-62659"
 dvf_path = ".\\Patients\\HN-CHUM-001\\08-27-1885-TomoTherapy Patient Disease-00441\\1-REGCTsim-CTPET-CT-43961"
 petct_path = ".\\Patients\\HN-CHUM-001\\08-27-1885-PANC. avec C.A. SPHRE ORL   tte et cou  -TP-74220\\3-StandardFull-07232"
+# Read series variables
+data_directory = os.path.dirname(".\\Patients\\HN-CHUM-001\\08-27-1885-TomoTherapy Patient Disease-00441\\112161818-kVCT Image Set-62659\\")
+series_ID = '1.3.6.1.4.1.14519.5.2.1.5168.2407.178959368858707198180439962659'
 
 ReadData = ReadData()
 ImageReg = ImageReg()
 
 
 def read_dicom(dicom_path):
+    # Function that reads a dicom file and writes to a text file
     ds = ReadData.read_dicom(dicom_path)
-    # dvf_data = ReadData.load_dvf_data(ds)
     ReadData.write_dicom(ds, "pct_dicom")
 
 
 def load_patients_array():
+    # Function that loads all important data for every patient
     pct_data = {}
     petct_data = {}
     dvf_data = {}
@@ -37,14 +41,17 @@ def load_patients_array():
 
 
 def main(argv=None):
-
-    pct_data, petct_data, dvf_data = ReadData.load_patient_array(dvf_path, pct_path, petct_path)
-    # ReadData.write_dicom(pct_data, 'pct_data')
-    # ReadData.write_dicom(petct_data, 'petct_data')
-    # ReadData.write_dicom(dvf_data, 'dvf_data')
-    pixel_array = pct_data["000000.dcm"]["Pixel Array"]
-    ImageReg.load_itk_image(pixel_array)
-
+    ImageReg.load_series(".\\Patients\\HN-CHUM-001\\08-27-1885-TomoTherapy Patient Disease-00441\\112161818-kVCT Image Set-62659\\", "pct_series")
+    ImageReg.load_series(".\\Patients\\HN-CHUM-001\\08-27-1885-PANC. avec C.A. SPHRE ORL   tte et cou  -TP-74220\\3-StandardFull-07232\\", "petct_series")
+    fixed = ImageReg.image_info("pct_series.mha")
+    moving = ImageReg.image_info("petct_series.mha")
+    img = ImageReg.resample_image(fixed, moving)
+    print("Size:", img.GetSize())
+    print("Origin:", img.GetOrigin())
+    print("Pixel Spacing:", img.GetSpacing())
+    print("Direction:", img.GetDirection())
+    # print(vis.shape())
+    ImageReg.myshow(img)
     print("Done Loading Patient Info")
 
 
