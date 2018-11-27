61c0f06a6d0fca953f00aa8a701f2d39461cb56a
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2018-11-26 23:28:55 -0500

    Mask Transforms, not working

--- Week_10/maskTransform.sh
+++ Week_10/maskTransform.sh
@@ -1,6 +1,8 @@
 echo \# Bash Script that transforms planning masks into PET masks
 
+cwd="/mnt/e/Mphys/"
 mask_out_dir="/mnt/e/Mphys/PatientMasks"
+mask_in_dir="/mnt/e/Mphys/PatientMasks_Backup"
 base_dir="/mnt/e/Mphys/NiftyPatients"
 fixed_ref="PlanningCT"
 moving_ref="PET"
@@ -12,20 +14,32 @@ fi
 if [ -d ${reg_out_dir}  ]; then
   rm -rf ${reg_out_dir}
 fi
-
+#  Create directories for Inverse Registration Images
 mkdir ${reg_out_dir}
-mkdir ${reg_out_dir}/PET/
+mkdir ${reg_out_dir}/Rigid/
+mkdir ${reg_out_dir}/Non-Rigid/
+mkdir ${reg_out_dir}/DVF/
+# Create directories for Mask Output
+mkdir ${mask_out_dir}
 mkdir ${mask_out_dir}/PET/
+mkdir ${mask_out_dir}/PET/Rigid/
+mkdir ${mask_out_dir}/PET/Non-Rigid/
 
-for filepath in `(ls -f ${out_dir}/${fixed_ref}/*.nii )`; do
+for filepath in `(ls -f ${mask_in_dir}/${fixed_ref}/*.nii )`; do
   #  Get patient name from filename
   filename=$(basename -- "$filepath")
   image="${filename%.*}"
   echo $image
-  mkdir ${out_dir}/PET/${image}
+  mkdir ${reg_out_dir}/Rigid/${image}
+  mkdir ${reg_out_dir}/Non-Rigid/${image}
+  mkdir ${reg_out_dir}/DVF/${image}
+  mkdir ${mask_out_dir}/PET/Rigid/${image}
+  mkdir ${mask_out_dir}/PET/Non-Rigid/${image}
   # Perform Registration, first affine then b-spline
   elastix -m ${base_dir}/${fixed_ref}/${image} -f ${base_dir}/${moving_ref}/${image} -p ${cwd}/Affine-parameter-file.txt  -out ${reg_out_dir}/Rigid/${image}
-  elastix -m ${base_dir}/${fixed_ref}/${image} -f ${out_dir}/Rigid/${image}/result.0.nii -p ${cwd}/B-Spline-parameter-file.txt  -out ${out_dir}/Non-Rigid/${image}
+  transformix -in ${mask_in_dir}/${fixed_ref}/${image} -out ${mask_out_dir}/${moving_ref}/Rigid/${image} -tp ${reg_out_dir}/Rigid/${image}/TransformParameters.0.txt
+  elastix -m ${reg_out_dir}/Rigid/${image}/result.0.nii -f ${base_dir}/${moving_ref}/${image} -p ${cwd}/B-Spline-parameter-file.txt  -out ${reg_out_dir}/Non-Rigid/${image} 
   # Generate deformation field from affine transform file
-  transformix -def all -tp ${out_dir}/Non-Rigid/${image}/TransformParameters.0.txt -out ${out_dir}/DVF/${image}
+  transformix -def all -tp ${reg_out_dir}/Non-Rigid/${image}/TransformParameters.0.txt -out ${reg_out_dir}/DVF/${image}
+  transformix -in ${mask_out_dir}/${moving_ref}/Rigid/${image}/result.nii -out ${mask_out_dir}/${moving_ref}/Non-Rigid/${image} -tp ${reg_out_dir}/Non-Rigid/${image}/TransformParameters.0.txt
 done
