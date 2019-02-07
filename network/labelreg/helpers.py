44b731e79660f2af01961f454c91cd2b99ad6542
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-02-07 16:52:05 +0000

    affine param

--- network/labelreg/helpers.py
+++ network/labelreg/helpers.py
@@ -136,7 +136,7 @@ def mod_write_images(input_, base_affine, file_path=None, file_prefix=''):
     if file_path is not None:
         batch_size = input_.shape[0]
         #affine = [base_affine[i].getaffine for i in range(base_affine.num_data)]
-        affine = [[10, 0, 0, 0], [0, 10, 0, 0], [0, 0, 10, 0], [0, 0, 0, 0]]
+        affine = [[-8, 0, 0, 260], [0, -8, 0, 220], [0, 0, 4, 130], [0, 0, 0, 1]]
         [nib.save(nib.Nifti1Image(input_[idx, ...], affine),
                   os.path.join(file_path,
                                file_prefix + '%s.nii' % idx))
