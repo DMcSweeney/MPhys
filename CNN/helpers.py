9fb001484a6db75a393da82cc5e3b16525b5d2c0
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-02-22 12:35:56 +0000

    Add inference files and helpers organisation

---
+++ CNN/helpers.py
@@ -0,0 +1,65 @@
+""" File with useful functions to call"""
+import numpy as np
+import nibabel as nib
+import os
+
+
+class Helpers(object):
+    @staticmethod
+    def generator(inputs, label, batch_size=4, predict=False):
+        x_dim, y_dim, z_dim, channel = inputs[0].shape[1:]
+        fixed_input, moving_input = inputs
+        batch_fixed, batch_moving = np.zeros((batch_size, x_dim, y_dim, z_dim, channel)), np.zeros(
+            (batch_size, x_dim, y_dim, z_dim, channel))
+        # add 3 due to 3D vector
+        batch_label = np.zeros((batch_size, x_dim, y_dim, z_dim, 3))
+        if predict is False:
+            while True:
+                for i in range(batch_size):
+                    # Random index from dataset
+                    index = np.random.choice(len(label), 1)
+                    batch_fixed[i], batch_moving[i] = inputs[0][index, ...], inputs[1][index, ...]
+                    batch_label[i] = label[index, ...]
+                yield ({'input_1': batch_fixed, 'input_2': batch_moving}, {'dvf': batch_label})
+        else:
+            while True:
+                for i in range(batch_size):
+                    # Random index from dataset
+                    index = np.random.choice(len(label), 1)
+                    batch_fixed[i], batch_moving[i] = inputs[0][index, ...], inputs[1][index, ...]
+                yield ({'input_1': batch_fixed, 'input_2': batch_moving})
+
+    @staticmethod
+    def shuffle_inplace(fixed, moving, dvf):
+        np.random.seed(1234)
+        assert len(fixed[:, ...]) == len(moving[:, ...]) == len(dvf[:, ...])
+        p = np.random.permutation(len(fixed[:, ...]))
+        print("Permutation:", p)
+        return fixed[p], moving[p], dvf[p]
+
+    @staticmethod
+    def split_data(fixed_image, moving_image, dvf_label, split_ratio):
+        validation_size = int(split_ratio*fixed_image.shape[0])
+
+        validation_fixed = fixed_image[:validation_size, ...]
+        validation_moving = moving_image[:validation_size, ...]
+        validation_dvf = np.squeeze(dvf_label[:validation_size, ...])
+
+        train_fixed = fixed_image[validation_size:, ...]
+        train_moving = moving_image[validation_size:, ...]
+        train_dvf = np.squeeze(dvf_label[validation_size:, ...])
+        print("Validation shape:", validation_fixed.shape)
+        print("Training shape:", train_fixed.shape)
+        return validation_fixed, validation_moving, validation_dvf, train_fixed, train_moving, train_dvf
+
+    @staticmethod
+    def write_images(input_, base_affine, file_path=None, file_prefix=''):
+        if file_path is not None:
+            batch_size = input_.shape[0]
+            print("affine shape", base_affine.shape)
+            print("Input shape", input_.shape)
+            #affine = [base_affine[idx] for idx in range(batch_size)]
+            [nib.save(nib.Nifti1Image(input_[idx, ...], base_affine[idx, ...]),
+                      os.path.join(file_path,
+                                   file_prefix + '%s.nii' % idx))
+             for idx in range(batch_size)]
---
+++ CNN/inference.py
@@ -0,0 +1,70 @@
+"""Inference file for CNN registration on unseen data"""
+from keras.models import load_model
+from helpers import Helpers
+import dataLoader as load
+import math
+
+helper = Helpers()
+
+
+# On server
+fixed_dir = "/hepgpu3-data1/dmcsween/DataWithInv/fixed"
+moving_dir = "/hepgpu3-data1/dmcsween/DataWithInv/moving"
+dvf_dir = "/hepgpu3-data1/dmcsween/DataWithInv/DVF"
+"""
+# On laptop
+fixed_dir = "E:/MPhys/Data128/PlanningCT"
+moving_dir = "E:/MPhys/Data128/PET_Rigid"
+dvf_dir = "E:/MPhys/Data128/DVF"
+"""
+batch_size = 1
+
+
+def inference():
+    print('Load data to Transform')
+    fixed_predict, moving_predict, dvf_label = load.data_reader(fixed_dir, moving_dir, dvf_dir)
+
+    print('Turn into numpy arrays')
+    fixed_array, fixed_affine = fixed_predict.get_data()
+    moving_array, moving_affine = moving_predict.get_data()
+    dvf_array, dvf_affine = dvf_label.get_data(is_image=False)
+
+    print("Fixed affine:", fixed_affine.shape)
+    print("DVF affine:", dvf_affine.shape)
+
+    print('Shuffle')
+    fixed_array, moving_array, dvf_array = helper.shuffle_inplace(
+        fixed_array, moving_array, dvf_array)
+    fixed_affine, moving_affine, dvf_affine = helper.shuffle_inplace(
+        fixed_affine, moving_affine, dvf_affine)
+
+    print('Split into test/training data')
+    test_fixed, test_moving, test_dvf, train_fixed, train_moving, train_dvf = helper.split_data(
+        fixed_array, moving_array, dvf_array, split_ratio=0.05)
+    test_fixed_affine, test_moving_affine, test_dvf_affine, train_fixed_affine, train_moving_affine, train_dvf_affine = helper.split_data(
+        fixed_affine, moving_affine, dvf_affine, split_ratio=0.05)
+
+    print('Load models')
+    print("Fixed input", test_fixed.shape)
+    print("Moving input", test_moving.shape)
+    model = load_model('best_model.h5')
+    model.compile(optimizer='Adam', loss='mean_squared_error', metrics=["accuracy"])
+    dvf = model.predict_generator(helper.generator([test_fixed, test_moving], label=test_dvf, predict=True), steps=math.ceil(
+        test_fixed.shape[0]/batch_size), verbose=1)
+
+    print('Save DVF')
+    # Save images
+    helper.write_images(test_fixed, test_fixed_affine, file_path='./outputs/', file_prefix='fixed')
+    helper.write_images(test_moving, test_moving_affine,
+                        file_path='./outputs/', file_prefix='moving')
+    helper.write_images(dvf, test_fixed_affine, file_path='./outputs/', file_prefix='dvf')
+
+    # Save warped
+
+
+def main(argv=None):
+    inference()
+
+
+if __name__ == '__main__':
+    main()
