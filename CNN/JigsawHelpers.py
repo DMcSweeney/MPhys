e1c351020ce4bf27ece4b4c61ec5c7a1e91c6e0b
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-03-03 23:58:52 +0000

    Add shuffling of array

--- CNN/JigsawHelpers.py
+++ CNN/JigsawHelpers.py
@@ -6,6 +6,7 @@ from itertools import product
 import dataLoader as load
 import helpers as help
 import math
+import random
 # On server
 fixed_dir = "/hepgpu3-data1/dmcsween/DataTwoWay128/fixed"
 moving_dir = "/hepgpu3-data1/dmcsween/DataTwoWay128/moving"
@@ -45,12 +46,22 @@ def jigsaw_mix(air_threshold=3):
 """
 
 
+def shuffle_jigsaw(input_dict, number_cells_per_dim=4):
+    # Randomly assign key to value
+    list_keys = []
+    for key in input_dict.keys():
+        list_keys.append(key)
+    shuffle_dict = {prod: input_dict[random_key] for random_key in random.sample(
+        list_keys, len(list_keys))for prod in product(range(0, number_cells_per_dim), repeat=dims)}
+    return shuffle_dict
+
+
 def solve_jigsaw(cells, input_array):
     puzzle_array = np.zeros(shape=input_array.shape)
     for key, value in cells.items():
         x, y, z = key
         puzzle_array[:, x*value.shape[1]:x*value.shape[1]+value.shape[1], y*value.shape[2]:y *
-                     value.shape[2]+value.shape[2], z*value.shape[3]:z*value.shape[3]+value.shape[3], :] = value
+                     value.shape[2]+value.shape[2], z*value.shape[3]: z*value.shape[3]+value.shape[3], :] = value
     return puzzle_array
 
 
@@ -69,9 +80,10 @@ def main(argv=None):
     fixed_array, fixed_affine = get_data(fixed_dir, moving_dir, dvf_dir)
     # Divide input_
     fixed_cells = divide_input(fixed_array)
+    shuffle_image = shuffle_jigsaw(fixed_cells)
     # Check shapes
-    puzzle_array = solve_jigsaw(fixed_cells, fixed_array)
-    help.write_images(puzzle_array, fixed_affine, file_path="./jigsaw_out/", file_prefix='test')
+    puzzle_array = solve_jigsaw(shuffle_image, fixed_array)
+    help.write_images(puzzle_array, fixed_affine, file_path="./jigsaw_out/", file_prefix='shuffle')
 
 
 if __name__ == '__main__':
