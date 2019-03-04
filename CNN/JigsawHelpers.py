29bdfc459dbeb8d782afb04061ffc7e1de67e6b5
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-03-04 14:52:14 +0000

    fix input array shape in dict comp

--- CNN/JigsawHelpers.py
+++ CNN/JigsawHelpers.py
@@ -36,12 +36,14 @@ def divide_input(input_array, number_cells_per_dim=4, dims=3):
     print("Input Dimensions:", input_array.ndim)
     if input_array.ndim == 5:
         sliced_dims = tuple([int(x/number_cells_per_dim) for x in input_array.shape[1:4]])
+        sliced_x, sliced_y, sliced_z = sliced_dims
+        cells = {prod: np.array(input_array[:, prod[0]*sliced_x: prod[0]*sliced_x+sliced_x, prod[1]*sliced_y: prod[1]*sliced_y+sliced_y, prod[2]*sliced_z: prod[2]*sliced_z+sliced_z, :])
+                 for prod in product(range(0, number_cells_per_dim), repeat=dims)}
     else:
         sliced_dims = tuple([int(x/number_cells_per_dim) for x in input_array.shape[:3]])
-    print("Sliced Dims:", sliced_dims)
-    sliced_x, sliced_y, sliced_z = sliced_dims
-    cells = {prod: np.array(input_array[:, prod[0]*sliced_x: prod[0]*sliced_x+sliced_x, prod[1]*sliced_y: prod[1]*sliced_y+sliced_y, prod[2]*sliced_z: prod[2]*sliced_z+sliced_z, :])
-             for prod in product(range(0, number_cells_per_dim), repeat=dims)}
+        sliced_x, sliced_y, sliced_z = sliced_dims
+        cells = {prod: np.array(input_array[prod[0]*sliced_x: prod[0]*sliced_x+sliced_x, prod[1]*sliced_y: prod[1]*sliced_y+sliced_y, prod[2]*sliced_z: prod[2]*sliced_z+sliced_z, :])
+                 for prod in product(range(0, number_cells_per_dim), repeat=dims)}
     return cells
 
 
