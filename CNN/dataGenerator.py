1e54d90df57aeafe3e14765b614fd7fd73a6739e
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-03-21 13:11:20 +0000

    change shape of array list

--- CNN/dataGenerator.py
+++ CNN/dataGenerator.py
@@ -13,10 +13,10 @@ moving_dir = "/hepgpu3-data1/dmcsween/DataTwoWay128/moving"
 dvf_dir = "/hepgpu3-data1/dmcsween/DataTwoWay128/DVF"
 
 
-def generator(image_array, avail_keys, hamming_set, batch_size=8, num_permutations=50):
+def generator(image_array, avail_keys, hamming_set, crop_size=25, batch_size=8, num_permutations=50):
     # Divide array into cubes
     idx_array = np.zeros((batch_size, hamming_set.shape[0]), dtype=np.uint8)
-    array_list = np.zeros((batch_size, len(avail_keys)))
+    array_list = np.zeros((batch_size, len(avail_keys), crop_size, crop_size, crop_size, 1))
     while True:
         for i in range(batch_size):
             rand_idx = random.randrange(image_array.shape[0])
@@ -31,8 +31,8 @@ def generator(image_array, avail_keys, hamming_set, batch_size=8, num_permutatio
             # Randomly assign labels to cells
             # print("Permutation:", hamming_set[random_idx])
             out_dict = help.shuffle_jigsaw(cropped_dict, hamming_set[random_idx])
-            for val in out_dict.values():
-                array_list[i, ...] = helper.normalise(val)
+            for n, val in enumerate(out_dict.values()):
+                array_list[i, n, ...] = helper.normalise(val)
             idx_array[i, random_idx] = 1
         yield ({'alexnet_input_{}'.format(n): elem for n, elem in enumerate(array_list)}, {'ClassificationOutput': idx_array})
 
