3bcc94a73559a95a5cec88ae0b89af46cbb22d45
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-03-29 15:28:28 +0000

    Use list comp for output predict_generator

--- CNN/dataGenerator.py
+++ CNN/dataGenerator.py
@@ -84,7 +84,7 @@ def predict_generator(image_array, avail_keys, hamming_set, hamming_idx=None, cr
                 array_list[i, n, ...] = val
             idx_array[i, random_idx] = 1
         # return array_list, idx_array, out_dict, fix_dict
-        yield [np.array([array_list[:, n, ...] for n in range(len(avail_keys))]), np.array(idx_array)]
+        yield [[array_list[:, n, ...] for n in range(len(avail_keys))], idx_array]
 
 
 def main(N=10, batch_size=2):
