d7c74b6ca5edc526c768fb818cdf69c23f83f8e6
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-04-04 11:41:12 +0100

    Use dropout

--- CNN/JigNet.py
+++ CNN/JigNet.py
@@ -114,8 +114,10 @@ def createSharedAlexnet3D_onemodel(input_shape=(25, 25, 25, 1), nInputs=23, ncla
                     for n in range(nInputs)]
     an3D = createAlexnet3D(input_shape)
     fc6 = Concatenate()([an3D(x) for x in input_layers])
-    fc7 = Dense(1024, activation='relu')(fc6)
-    fc8 = Dense(nclass, activation='softmax', name="ClassificationOutput")(fc7)
+    dp1 = Dropout(0.8)(fc6)
+    fc7 = Dense(1024, activation='relu')(dp1)
+    dp2 = Dropout(0.8)(fc7)
+    fc8 = Dense(nclass, activation='softmax', name="ClassificationOutput")(dp2)
     model = Model(inputs=input_layers, output=fc8)
     return model
 
@@ -134,7 +136,7 @@ def train(tileSize=64, numPuzzles=23, num_permutations=1000, batch_size=32):
     # Get hamming set
     print("Load hamming Set")
     hamming_set = pd.read_csv(
-        "/hepgpu3-data1/heyst/MPhys/CNN/hamming_set.txt", sep=",", header=None)
+        "hamming_set.txt", sep=",", header=None)
     # print(hamming_set)
 
     fixed_array, moving_array, dvf_array = helper.shuffle_inplace(
@@ -168,12 +170,12 @@ def train(tileSize=64, numPuzzles=23, num_permutations=1000, batch_size=32):
                   metrics=['accuracy'])
 
     model.fit_generator(generator=gen.generator(normalised_train, list_avail_keys, hamming_set, batch_size=batch_size, N=num_permutations),
-                        epochs=1000, verbose=1,
+                        epochs=20, verbose=1,
                         steps_per_epoch=10,
                         validation_data=gen.generator(
         normalised_val, list_avail_keys, hamming_set, batch_size=batch_size, N=num_permutations),
         validation_steps=10, callbacks=callbacks)
-    model.save('model_10.h5')
+    model.save('model_11.h5')
 
 
 """
