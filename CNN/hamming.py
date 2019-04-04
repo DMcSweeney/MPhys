f7cd7d7c155955a3cb080597218408d5c945b2b2
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-04-04 17:22:28 +0100

    hamming with 24 cubes

--- CNN/hamming.py
+++ CNN/hamming.py
@@ -130,12 +130,12 @@ def TOM_gen_max_hamming_set(N, moving_cells):
 
 def main(argv=None):
 
-    N = 1000
-    avail_keys = [n for n in range(23)]
+    N = 10
+    avail_keys = [n for n in range(24)]
     print(avail_keys)
-    max_dist_set, dist_array = TOM_gen_max_hamming_set(N, avail_keys)
-    np.savetxt("hamming_set.txt", max_dist_set, delimiter=",", fmt='%1.2i')
-    np.savetxt("hamming_min_mean_1000.txt", dist_array,delimiter=",", fmt='%1.2i'  )
+    max_dist_set, dist_array = gen_max_hamming_set(N, avail_keys)
+    np.savetxt("hamming_set_PCT.txt", max_dist_set, delimiter=",", fmt='%1.2i')
+    #np.savetxt("hamming_min_mean_1000.txt", dist_array, delimiter=",", fmt='%1.2i')
 
     """
 
@@ -146,5 +146,7 @@ def main(argv=None):
     print("Sliced", df)
 
     """
+
+
 if __name__ == '__main__':
     main()
