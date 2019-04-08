c5fe5f81a6ba9859803bae83f67b7c23b376c983
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-04-08 19:08:11 +0100

    hamming_idx=0

--- CNN/JigNet.py
+++ CNN/JigNet.py
@@ -168,11 +168,11 @@ def train(tileSize=64, numPuzzles=24, num_permutations=10, batch_size=32):
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
-    model.fit_generator(generator=gen.generator(normalised_train, list_avail_keys, hamming_set, img_idx=0, batch_size=batch_size, N=num_permutations),
+    model.fit_generator(generator=gen.generator(normalised_train, list_avail_keys, hamming_set, img_idx=0, hamming_idx=0, batch_size=batch_size, N=num_permutations),
                         epochs=50, verbose=1,
                         steps_per_epoch=10,
                         validation_data=gen.generator(
-        normalised_train, list_avail_keys, hamming_set, img_idx=0, batch_size=batch_size, N=num_permutations),
+        normalised_train, list_avail_keys, hamming_set, img_idx=0, hamming_idx=0, batch_size=batch_size, N=num_permutations),
         validation_steps=10, callbacks=callbacks, shuffle=False)
     model.save('model_one_img.h5')
 
