b91949372f6759d305df8505df08b102c53730a9
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-04-08 21:43:38 +0100

    activation to sigmoid

--- CNN/JigNet.py
+++ CNN/JigNet.py
@@ -89,37 +89,39 @@ def createSharedAlexnet3D(tileSize=25, numPuzzles=23, hammingSetSize=10):
 
 
 def createAlexnet3D(input_shape=(25, 25, 25, 1)):
-    kernel_initializer = 'he_normal'
+    kernel_initializer = 'RandomNormal'
+    activation = 'sigmoid'
     inputLayer = Input(shape=(input_shape))
-    x = Conv3D(96, (11, 11, 11), strides=(2, 2, 2), activation='relu',
+    x = Conv3D(96, (11, 11, 11), strides=(2, 2, 2), activation=activation,
                padding='valid', kernel_initializer=kernel_initializer)(inputLayer)  # Note, strides are different!
     x = BatchNormalization()(x)
-    x = Conv3D(256, (5, 5, 5), strides=(1, 1, 1), activation='relu',
+    x = Conv3D(256, (5, 5, 5), strides=(1, 1, 1), activation=activation,
                padding='same', kernel_initializer=kernel_initializer)(x)
     x = BatchNormalization()(x)
     x = MaxPooling3D(pool_size=(3, 3, 3), strides=(2, 2, 2))(x)
-    x = Conv3D(384, (3, 3, 3), activation='relu', padding='same',
+    x = Conv3D(384, (3, 3, 3), activation=activation, padding='same',
                kernel_initializer=kernel_initializer)(x)
     x = BatchNormalization()(x)
     x = MaxPooling3D(pool_size=(3, 3, 3), strides=(2, 2, 2))(x)
     x = Conv3D(384, (3, 3, 3), strides=(1, 1, 1), padding='same',
-               kernel_initializer=kernel_initializer)(x)
+               kernel_initializer=kernel_initializer, activation=activation)(x)
     x = BatchNormalization()(x)
-    x = Conv3D(256, (3, 3, 3), padding='same', kernel_initializer=kernel_initializer)(x)
+    x = Conv3D(256, (3, 3, 3), padding='same',
+               kernel_initializer=kernel_initializer, activation=activation)(x)
     x = BatchNormalization()(x)
     x = Flatten()(x)
-    outputLayer = Dense(1024, activation='relu')(x)
+    outputLayer = Dense(1024, activation=activation)(x)
     an3D = Model(inputs=[inputLayer], outputs=outputLayer)
     return an3D
 
 
 def createSharedAlexnet3D_onemodel(input_shape=(25, 25, 25, 1), nInputs=24, nclass=10):
-
+    activation = 'sigmoid'
     input_layers = [Input(shape=input_shape, name="alexnet_input_{}".format(n))
                     for n in range(nInputs)]
     an3D = createAlexnet3D(input_shape)
     fc6 = Concatenate()([an3D(x) for x in input_layers])
-    fc7 = Dense(1024, activation='relu')(fc6)
+    fc7 = Dense(1024, activation=activation)(fc6)
     fc8 = Dense(nclass, activation='softmax', name="ClassificationOutput")(fc7)
     model = Model(inputs=input_layers, output=fc8)
     return model
