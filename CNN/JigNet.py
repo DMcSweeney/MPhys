4970255ef545622d611d85a05ee88f4b3e0fc93d
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-04-09 14:08:54 +0100

    SGD

--- CNN/JigNet.py
+++ CNN/JigNet.py
@@ -189,8 +189,7 @@ def train(tileSize=64, numPuzzles=24, num_permutations=10, batch_size=16):
     model = createSharedAlexnet3D_onemodel()
     # for layer in model.layers:
     #     print(layer.name, layer.output_shape)
-    #opt = optimizers.SGD(lr=0.01)
-    opt = optimizers.Adadelta()
+    opt = optimizers.SGD(lr=0.01)
     plot_model(model, to_file='model.png')
     print(model.summary())
     model.compile(optimizer=opt,
