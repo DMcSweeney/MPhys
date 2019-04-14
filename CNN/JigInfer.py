5ac6c4d081d04aa3ee5cbfb410e6314899f28aee
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-04-14 12:38:42 +0100

    Load model_best and format predicted out

--- CNN/JigInfer.py
+++ CNN/JigInfer.py
@@ -11,7 +11,7 @@ import numpy as np
 
 def infer(batch_size=2):
     # On server with PET and PCT in
-    #image_dir = "/hepgpu3-data1/dmcsween/DataTwoWay128/fixed"
+    # image_dir = "/hepgpu3-data1/dmcsween/DataTwoWay128/fixed"
     image_dir = "/hepgpu3-data1/dmcsween/Data128/ResampleData/PlanningCT"
     print("Load Data")
     image_data, __image, __label = load.data_reader(image_dir, image_dir, image_dir)
@@ -45,16 +45,16 @@ def infer(batch_size=2):
     print('Load models')
     idx_list = [10, 12]
     K.clear_session()
-    model = load_model('./logs/best_model.h5')
+    model = load_model('./model_best.h5')
     myPredictGen = gen.predict_generator(
         normalised_dataset, list_avail_keys, hamming_set, hamming_idx=idx_list, batch_size=batch_size, N=10)
     opt = optimizers.SGD(lr=0.01, momentum=0.9)
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=["accuracy"])
     output = model.predict_generator(generator=myPredictGen, steps=1, verbose=1)
     # print(output)
-    for img in output:
+    for i, img in enumerate(output):
         print(img)
-        print(np.argmax(img))
+        print("Predicted index:{}. Should be: {}".format(np.argmax(img), idx_list[i])
 
 
 def main(argv=None):
