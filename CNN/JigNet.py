78529ab5bf45032e7dea3b0363fd79420cd6801f
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-04-27 13:22:58 +0100

    pet with 100 perms

--- CNN/JigNet.py
+++ CNN/JigNet.py
@@ -123,7 +123,7 @@ def createNet(input_shape=(28, 28, 28, 1)):
     return an3D
 
 
-def createSharedAlexnet3D_onemodel(input_shape=(28, 28, 28, 1), nInputs=23, nclass=50):
+def createSharedAlexnet3D_onemodel(input_shape=(28, 28, 28, 1), nInputs=23, nclass=100):
     activation = 'relu'
     input_layers = [Input(shape=input_shape, name="alexnet_input_{}".format(n))
                     for n in range(nInputs)]
@@ -136,7 +136,7 @@ def createSharedAlexnet3D_onemodel(input_shape=(28, 28, 28, 1), nInputs=23, ncla
     return model
 
 
-def train(tileSize=64, numPuzzles=23, num_permutations=50, batch_size=16):
+def train(tileSize=64, numPuzzles=23, num_permutations=100, batch_size=16):
     # On server with PET and PCT in
     #image_dir = "/hepgpu3-data1/dmcsween/Data128/ResampleData/PlanningCT"
     image_dir = "/hepgpu3-data1/dmcsween/Data128/ResampleData/PET_Rigid"
@@ -162,7 +162,7 @@ def train(tileSize=64, numPuzzles=23, num_permutations=50, batch_size=16):
     hamming_set = pd.read_csv(
         "mixed_hamming_set.txt", sep=",", header=None)
 
-    hamming_set = hamming_set.loc[:49]
+    hamming_set = hamming_set.loc[:99]
     print("Ham Len", len(hamming_set))
     # print(hamming_set)
 
