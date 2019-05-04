21d1effb03484f2c8a8a2a1ac5c88d282b0f12d0
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-05-04 19:14:05 +0100

    loop around input to blank

--- CNN/JigInfer.py
+++ CNN/JigInfer.py
@@ -42,29 +42,31 @@ def infer(batch_size=2):
     print("Valid Shape:", test_dataset.shape)
     normalised_dataset = helper.normalise(test_dataset)
     print('Load models')
-    idx_list = [12, 38]
-    blank_idx = [0]
-    #scores = []
-
+    idx_list = [12]
+    scores = []
     K.clear_session()
     model = load_model(inputPath + '/best_model.h5')
-    myPredictGen = gen.evaluate_generator(
-        normalised_dataset, list_avail_keys, hamming_set, hamming_idx=idx_list, batch_size=batch_size, blank_idx=blank_idx, N=100)
     opt = optimizers.SGD(lr=0.01)
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=["accuracy"])
-    print("Pre Eval")
-    """
-    output = model.predict_generator(generator=myPredictGen, steps=1, verbose=1)
-    for i, img in enumerate(output):
-        print(img)
-        print("Predicted index:{}. Should be: {}".format(np.argmax(img), idx_list[i]))
-        print("Accuracy: {}".format(np.max(img)))
-    """
-    accuracy = model.evaluate_generator(generator=myPredictGen, steps=5, verbose=1)
-    print(model.metrics_names)
-    print("%s: %.2f%%" % (model.metrics_names[1], accuracy[1]*100))
+    for i in range(22):
+        blank_idx = [i]
+        print("Pre Eval")
+        myPredictGen = gen.evaluate_generator(
+            normalised_dataset, list_avail_keys, hamming_set, hamming_idx=idx_list, batch_size=batch_size, blank_idx=blank_idx, N=100)
+        accuracy = model.evaluate_generator(generator=myPredictGen, steps=5, verbose=1)
+        print("%s: %.2f%%" % (model.metrics_names[1], accuracy[1]*100))
+        scores.append(accuracy[1] * 100)
+    print(scores)
     print("Done")
-    #scores.append(accuracy[1] * 100)
+
+
+"""
+output = model.predict_generator(generator=myPredictGen, steps=1, verbose=1)
+for i, img in enumerate(output):
+    print(img)
+    print("Predicted index:{}. Should be: {}".format(np.argmax(img), idx_list[i]))
+    print("Accuracy: {}".format(np.max(img)))
+"""
 
 
 def main(argv=None):
