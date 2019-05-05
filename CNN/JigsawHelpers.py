16ee778daef3b36ef9f8feef0e9ccb75909316f5
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-05-05 20:38:20 +0100

    remove zeroes

--- CNN/JigsawHelpers.py
+++ CNN/JigsawHelpers.py
@@ -55,7 +55,7 @@ def outer_crop(input_dict, blank_key, crop_size=14, border_size=7):
                          border_size:border_size+crop_size, border_size:border_size+crop_size, :]
             print(np.mean(centre))
             print(val.shape)
-            val[...] = np.zeros(shape=val.shape)
+            #val[...] = np.zeros(shape=val.shape)
             val[:, border_size:border_size+crop_size, border_size:border_size +
                 crop_size, border_size:border_size+crop_size, :] = centre
 
