e33d60e3a966f64ac20784e044c2aec854d9a29d
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-05-07 14:57:47 +0100

    remove normalise from train gen

--- CNN/dataGenerator.py
+++ CNN/dataGenerator.py
@@ -56,7 +56,7 @@ def generator(image_array, avail_keys, hamming_set, img_idx=None, hamming_idx=No
             out_dict = help.shuffle_jigsaw(cropped_dict, hamming_set.loc[random_idx, :].values)
 
             for n, val in enumerate(out_dict.values()):
-                array_list[i, n, ...] = helper.norm(val)
+                array_list[i, n, ...] = val
             idx_array[i, random_idx] = 1
 
         inputs = [array_list[:, n, ...] for n in range(len(avail_keys))]
