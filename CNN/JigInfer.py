f1d78f86584e6a7b4c55762a0164f8fb4bd58d9c
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-05-09 17:14:42 +0100

    switch order

--- CNN/JigInfer.py
+++ CNN/JigInfer.py
@@ -49,11 +49,12 @@ def infer(batch_size=2):
     model = load_model(inputPath + '/final_model.h5')
     opt = optimizers.SGD(lr=0.01)
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=["accuracy"])
-    for j in range(10):
-        idx_list = [j, j]
-        for i in range(23):
-            blank_idx.append(i)
-            print("Pre Eval")
+    idx_list = []
+    for i in range(23):
+        blank_idx.append(i)
+        for j in range(10):
+            idx_list = [j, j]
+            print("Pre Eval:", i, j)
             myPredictGen = gen.evaluate_generator(
                 normalised_dataset, list_avail_keys, hamming_set, hamming_idx=idx_list, batch_size=batch_size, blank_idx=blank_idx, full_crop=True, out_crop=False, inner_crop=False, N=100)
             accuracy = model.evaluate_generator(generator=myPredictGen, steps=5, verbose=1)
