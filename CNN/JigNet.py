ce8c2daa7ec45f9a82328fecba8f9a157e70c03f
Thomas Heys <heys.thomas@gmail.com>
2019-05-13 14:55:28 +0100

    2 of 5

--- CNN/JigNet.py
+++ CNN/JigNet.py
@@ -191,14 +191,14 @@ def train(tileSize=64, numPuzzles=23, num_permutations=100, batch_size=16):
 
     X = conc_data
 
-    i = 1
+    i = 2
     indexPath = './indices/'
 
 
 
-    trainIndex = pd.read_csv(indexPath + "train_0.txt", sep=",", header=None)
+    trainIndex = pd.read_csv(indexPath + "train_1.txt", sep=",", header=None)
     trainIndex = trainIndex.values
-    testIndex = pd.read_csv(indexPath + "test_0.txt", sep=",", header=None)
+    testIndex = pd.read_csv(indexPath + "test_1.txt", sep=",", header=None)
     testIndex = testIndex.values
 
     trainData = X[trainIndex]
