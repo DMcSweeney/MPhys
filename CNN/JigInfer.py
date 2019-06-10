52989e50d6371d7798509f494da5d694e9eef111
DMcSweeney <37912712+DMcSweeney@users.noreply.github.com>
2019-06-10 14:43:13 +0100

    change score array dims

--- CNN/JigInfer.py
+++ CNN/JigInfer.py
@@ -43,8 +43,9 @@ def infer(batch_size=2):
     print("Valid Shape:", test_dataset.shape)
     normalised_dataset = helper.normalise(test_dataset)
     print('Load models')
-    scores = np.zeros((23, 10))
+    scores = np.zeros((14, 10))
     blank_idx = [n for n in range(23)]
+    print(blank_idx)
     K.clear_session()
     model = load_model(inputPath + '/final_model.h5')
     opt = optimizers.SGD(lr=0.01)
